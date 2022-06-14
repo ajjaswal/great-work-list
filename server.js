a0805046fc26c81d84a6c4463efa438cb15e3703
AJ Jaswal <jaswalakshay@yahoo.com>
2022-06-14 19:25:22 -0400

    option to update employee role status

--- server.js
+++ server.js
@@ -252,4 +252,42 @@ const roleAdd = async () => {
       initialAction();
   };
 }
-
+const employeeUpdate = async () => {
+  try {
+      console.log('Employee Update');
+      let employees = await connection.query("SELECT * FROM employee");
+      let employeeSelection = await inquirer.prompt([
+          {
+              name: 'employee',
+              type: 'list',
+              choices: employees.map((employeeName) => {
+                  return {
+                      name: employeeName.first_name + " " + employeeName.last_name,
+                      value: employeeName.id
+                  }
+              }),
+              message: 'Which employee do you want to update?'
+          }
+      ]);
+      let roles = await connection.query("SELECT * FROM role");
+      let roleSelection = await inquirer.prompt([
+          {
+              name: 'role',
+              type: 'list',
+              choices: roles.map((roleName) => {
+                  return {
+                      name: roleName.title,
+                      value: roleName.id
+                  }
+              }),
+              message: 'Please select a role.'
+          }
+      ]);
+      let result = await connection.query("UPDATE employee SET ? WHERE ?", [{ role_id: roleSelection.role }, { id: employeeSelection.employee }]);
+      console.log(`The role was successfully updated.\n`);
+      initialAction();
+  } catch (err) {
+      console.log(err);
+      initialAction();
+  };
+}
