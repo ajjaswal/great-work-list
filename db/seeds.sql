f467d6333be41d3154851d1ae2c1444f455e85d1
AJ Jaswal <jaswalakshay@yahoo.com>
2022-06-14 20:12:13 -0400

    roles fixed

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,20 +1,20 @@
 INSERT INTO department (department_name)
-VALUES ("engineering"),
-    ("finance"),
+VALUES ("Operations"),
+    ("financial services"),
     ("marketing"),
     ("sales");
 
 SELECT * FROM department;
 
 INSERT INTO role (title, salary, department_id)
-VALUES ("software engineer", 120000, 1),
-    ("project manager", 90000, 1),
-    ("engineering manager", 225000, 1),
-    ("accountant", 70000, 2),
-    ("accounting manager", 120000, 2),
-    ("product marketing manager", 50000, 3),
+VALUES ("advisor support", 80000, 1),
+    ("accountant", 90000, 1),
+    ("Director", 100000, 1),
+    ("Advisor", 200000, 2),
+    ("Paraplanner", 100000, 2),
+    ("product marketing manager", 75000, 3),
     ("marketing lead", 150000, 3),
-    ("sales rep", 85000, 4);
+    ("sales representative", 85000, 4);
 
 SELECT * FROM ROLE;
 
